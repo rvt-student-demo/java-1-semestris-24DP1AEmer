9f54052677a6f67a39937ba22f799abcdab24246
24DP1AEmer <a240174ae@rvt.lv>
2025-09-18 12:40:40 +0300

    Odd or even

---
+++ src/main/java/rvt/Odd.java
@@ -0,0 +1,20 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Odd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        if (number % 2 == 0) {
+            System.out.println("number is even.");
+        } else {
+            System.out.println("number is odd.");
+        }
+
+        scanner.close();
+    }
+}
