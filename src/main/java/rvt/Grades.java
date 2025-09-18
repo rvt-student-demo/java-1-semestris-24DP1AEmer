b44d5490a181c752298ad2e89ac5173feaaf3355
24DP1AEmer <a240174ae@rvt.lv>
2025-09-18 12:33:46 +0300

    Grades and Points

---
+++ src/main/java/rvt/Grades.java
@@ -0,0 +1,40 @@
+package rvt;
+import java.util.Scanner;
+
+public class Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+            System.out.println("Give points [0-100]:");
+            int grade = Integer.valueOf(scanner.nextLine());
+
+            if (grade < 0) {
+                System.out.println("impossible!");
+            }
+
+            else if (grade < 50) {
+                System.out.println("failed");
+            }
+            else if (grade < 60) {
+                System.out.println("1");
+            }
+            else if (grade < 70) {
+                System.out.println("2");
+            }
+            else if (grade < 80) {
+                System.out.println("3");
+            }
+            else if (grade < 90) {
+                System.out.println("4");
+            }
+            else if (grade <= 100) {
+                System.out.println("5");
+            }
+            else {
+                System.out.println("incredible!");
+            }
+
+        scanner.close();
+    }
+}
