01be80a12b206b806820bfd465508e237dd857dc
Arturs Emerbergs <a240174@rvt.lv>
2025-10-03 19:53:59 +0300

    Astrelogia: Part 2

--- src/main/java/rvt/astrology.java
+++ src/main/java/rvt/astrology.java
@@ -1,14 +1,14 @@
 package rvt;
 
-public class astrology {
+import java.util.Scanner;
 
+public class astrology {
 
     public static void printSpaces(int number) {
         for (int i = 0; i < number; i++) {
             System.out.print(" ");
         }
     }
-
 
     public static void printStars(int number) {
         for (int i = 0; i < number; i++) {
@@ -17,9 +17,24 @@ public class astrology {
         System.out.println();
     }
 
+    public static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
     public static void main(String[] args) {
-        // example
-        printSpaces(5);
-        printStars(3);
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        printTriangle(size);
+
+        scanner.close();
     }
 }
+
+    
+
+    
