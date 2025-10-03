07d7c93256af97b45be742a533b8b19713225156
Arturs Emerbergs <a240174@rvt.lv>
2025-10-03 19:57:48 +0300

    Astrelogia: Part 3

--- src/main/java/rvt/astrology.java
+++ src/main/java/rvt/astrology.java
@@ -17,19 +17,22 @@ public class astrology {
         System.out.println();
     }
 
-    public static void printTriangle(int size) {
-        for (int i = 1; i <= size; i++) {
-            printSpaces(size - i);
-            printStars(i);
+    public static void christmasTree(int height) {
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+        for (int j = 0; j < 2; j++) {
+            printSpaces(height - 2);
+            printStars(3);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int height = scanner.nextInt();
 
-        int size = scanner.nextInt();
-
-        printTriangle(size);
+        christmasTree(height);
 
         scanner.close();
     }
