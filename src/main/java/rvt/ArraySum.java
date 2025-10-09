806ef705378e66801ecdc6ac80e4b67069930d1d
24DP1AEmer <a240174ae@rvt.lv>
2025-10-09 12:46:40 +0300

    Chapter 60

---
+++ src/main/java/rvt/ArraySum.java
@@ -0,0 +1,18 @@
+package rvt;
+public class ArraySum {
+    class Exercise1
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {0, 1, 2, 3}; 
+
+    int sum = 0;
+    for (int i = 0; i < val.length; i++) {
+        sum += val[i];
+    }
+ 
+    System.out.println( "Sum of all numbers = " + sum );
+ 
+   }
+}
+}
---
+++ src/main/java/rvt/TwoArray.java
@@ -0,0 +1,22 @@
+package rvt;
+public class TwoArray
+{ 
+  public static void main ( String[] args )
+  {
+    int[] val = {13, -4, 82, 17}; 
+    int[] twice = new int[val.length];
+    
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+
+    for (int i = 0; i < val.length; i++) {
+        twice[i] = 2 * val[i];
+    }
+
+
+ 
+    System.out.println( "New Array: " 
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+   }
+}
